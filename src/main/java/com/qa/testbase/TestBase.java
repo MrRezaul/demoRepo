b798913d00baf15a59e61cf0bb9ce44ba425e050
rezaul <mr.rezaul94@gmail.com>
2018-05-29 13:25:21 -0400

    my first git commit

---
+++ src/main/java/com/qa/testbase/TestBase.java
@@ -0,0 +1,9 @@
+package com.qa.testbase;
+
+public class TestBase {
+	@Test
+	public void Test() {
+	System.out.println("hello");
+
+}
+}
---
+++ src/test/java/com/qa/github/test/LoginTest.java
@@ -0,0 +1,5 @@
+package com.qa.github.test;
+
+public class LoginTest {
+
+}
